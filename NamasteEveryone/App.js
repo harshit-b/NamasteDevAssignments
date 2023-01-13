207d35117d72bc2db6073bed29023ecd4bfc807c
Harshit Bhargava <harshit.bhargava@ttu.edu>
2023-01-13 09:32:12 -0600

    Parce, Production and Development Build

--- NamasteEveryone/App.js
+++ NamasteEveryone/App.js
@@ -1,5 +1,16 @@
-const heading = React.createElement("h1", { id: "heading" }, "Namaste!");
-const heading1 = React.createElement("h2", { id: "heading1" }, "Everone!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", key: 1 },
+  "Namaste!"
+);
+const heading1 = React.createElement(
+  "h2",
+  { id: "heading1", key: 2 },
+  "Everone!"
+);
 const container = React.createElement("div", { id: "container" }, [
   heading,
   heading1,
